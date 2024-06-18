996837682c49c6340f876c6dbacf594d2c5499eb
nanashi <jose09_rbb@homtail.com>
2024-06-18 23:51:57 +0200

    [UPD] Updated docker compose file for deploy

--- src/main/java/com/nanashi/lichi/controller/UserController.java
+++ src/main/java/com/nanashi/lichi/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class UserController {
 
     private final UserRepository userRepository;
